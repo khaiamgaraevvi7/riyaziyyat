b14472fc56810d0b2ab9d0ea111902a68d68582d
Khaiam Garaevvi <khaiamgaraevvi1@gmail.com>
2025-02-02 17:26:25 +0400

    Update ebob_ve_ekob.py

--- ebob_ve_ekob.py
+++ ebob_ve_ekob.py
@@ -1,16 +1,16 @@
-sayi1 = int(input("Birinci sayiyi girin: "))
-sayi2 = int(input("Ikinci sayiyi girin: "))
+eded1 = int(input("Birinci ədədi yazın: "))
+eded2 = int(input("Ikinci ədədi yazın: "))
 
-if sayi1 > sayi2:
-    kucuk = sayi2
+if eded1 > eded2:
+    kicik = eded2
 else:
-    kucuk = sayi1
+    kicik = eded1
 
-for i in range(1, kucuk + 1):
-    if sayi1 % i == 0 and sayi2 % i == 0:
+for i in range(1, kicik + 1):
+    if eded1 % i == 0 and eded2 % i == 0:
         ebob = i
 
-ekok = (sayi1 * sayi2) // ebob
+ekob = (eded1 * eded2) // ebob
 
 print(f"EBOB: {ebob}")
-print(f"EKOK: {ekok}")
+print(f"EKOB: {ekok}")
